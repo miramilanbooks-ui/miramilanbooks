01fad69269870639e4dad373a1adf5842eac0572
miramilanbooks-ui <miramilanbooks@gmail.com>
2026-08-29 12:03:23 +0200

    Serve HD classroom image through email endpoint

--- netlify/functions/email-c-image.js
+++ netlify/functions/email-c-image.js
@@ -1,16 +1,33 @@
-const p1 = require('./email-c-image-part1');
-const p2 = require('./email-c-image-part2');
-const p3 = require('./email-c-image-part3');
-const p4 = require('./email-c-image-part4');
-const p5 = require('./email-c-image-part5');
+exports.handler = async () => {
+  try {
+    const source = 'https://miramilanbooks.com/email-c-classroom-activity-hires.jpg';
+    const response = await fetch(source, { headers: { 'User-Agent': 'MiraMilanBooks-EmailImage/1.0' } });
 
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: {
-    'Content-Type': 'image/jpeg',
-    'Cache-Control': 'public, max-age=31536000, immutable',
-    'Access-Control-Allow-Origin': '*'
-  },
-  body: p1 + p2 + p3 + p4 + p5,
-  isBase64Encoded: true
-});
+    if (!response.ok) {
+      return {
+        statusCode: 502,
+        headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+        body: 'Unable to load email image.'
+      };
+    }
+
+    const bytes = Buffer.from(await response.arrayBuffer());
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'image/jpeg',
+        'Cache-Control': 'public, max-age=31536000, immutable',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: bytes.toString('base64'),
+      isBase64Encoded: true
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+      body: 'Email image service error.'
+    };
+  }
+};
